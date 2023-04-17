3a41b88a03eb4089ff202ffc58b49c0720ff29bc
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-04-17 09:47:30 +0100

    about section

---
+++ src/component/lib/room-data.js
@@ -0,0 +1,23 @@
+export const roomData = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/id/954961748/photo/clean-open-space-interior.jpg?s=612x612&w=is&k=20&c=aIxydh2M_qsEfBz9JkJg6_qJDDXwGuKkH5ZTgMjRn50=",
+    title: "basic economy",
+    info: "Lorem Ipsum dolor sit amet consectetur adipising elit. Est quisquam corrupti dicta tempora sequi sapiente",
+    price: 125,
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/id/1434111874/photo/light-studio-interior-with-relax-and-dining-area-with-panoramic-window.jpg?s=612x612&w=is&k=20&c=wIVbpfPd4iuyngU06mq4iiscGcEO3F98D3l6Q6vR8d4=",
+    title: "presdential suite ",
+    info: "Lorem Ipsum dolor sit amet consectetur adipising elit. Est quisquam corrupti dicta tempora sequi sapiente",
+    price: 250,
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/id/1345809799/photo/modern-apartment-living-room-interior.jpg?s=1024x1024&w=is&k=20&c=IxAhgJAFFLEVJncC2HpqmfjdEkd8VJzUipIZw0MTucc=",
+    title: "standard queen",
+    info: "Lorem Ipsum dolor sit amet consectetur adipising elit. Est quisquam corrupti dicta tempora sequi sapiente",
+    price: 125,
+  },
+];
